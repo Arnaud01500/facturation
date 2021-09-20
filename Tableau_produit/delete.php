35bc9ecd41638a658d0a43e317301b0f446e373b
Arnaud01500 <arnaud.lyard@gmail.com>
2021-09-20 07:27:15 +0200

    json

--- Tableau_produit/delete.php
+++ Tableau_produit/delete.php
@@ -7,7 +7,6 @@ $idTemp = $_POST['product_code_r'];
 
 $product_code = json_decode($idTemp);
 
-
 //Traitement des données
 $query = "DELETE FROM products WHERE product_code='$product_code'";
 $result = $link->query($query);
